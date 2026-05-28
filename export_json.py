d52dee32570380f0e6bc82a97e1869d41566131a
Sophian Sefiani <sophiansimplon@gmail.com>
2026-05-28 09:42:39 +0200

    ajout dossier sophian

---
+++ export_json.py
@@ -0,0 +1,18 @@
+import sqlite3
+import json
+#Connexion à la base
+conn = sqlite3.connect("produits.db")
+cur = conn.cursor()
+# Exécuter une requête
+cur.execute("SELECT * FROM produits")
+rows = cur.fetchall()
+# Récupérer les noms de colonnes
+colonnes = [desc[0] for desc in cur.description]
+#Transformer en dictionnaires
+donnees = []
+for ligne in rows:
+    donnees.append(ligne)
+#Exporter en JSON
+with open("produits.json", "w", encoding="utf-8") as f:
+    json.dump(donnees, f, indent=4, ensure_ascii=False)
+print("Export terminé.")
